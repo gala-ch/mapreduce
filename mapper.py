7bd70836386a30fb92c08dd9708183a991c1c36b
gala-ch <s_chaparrovilanov22@stud.hwr-berlin.de>
2023-06-22 12:17:39 +0200

    ex3

--- mapper.py
+++ mapper.py
@@ -34,6 +34,10 @@ for line in sys.stdin:
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
 
+    # Check if there are six elements in the data list
+    if len(data) != 6:
+    	raise ValueError("Input data does not contain six elements.")
+
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
 
