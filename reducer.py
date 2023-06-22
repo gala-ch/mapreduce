97e99b24cd771eec35e9508113d9b2e0d6ad122b
gala-ch <s_chaparrovilanov22@stud.hwr-berlin.de>
2023-06-22 12:43:06 +0200

    ex6

--- reducer.py
+++ reducer.py
@@ -19,8 +19,9 @@ import sys
 # Cash  455.51
 
 # Sum of all sales (values) is initialized with zero, we just started
-purchase_count = 0
-
+sum_of_values = 0
+# Count of values for each category
+count = 0
 # Previous key is initialized with None, we just started
 previous_key = None
 
@@ -32,28 +33,36 @@ for line in sys.stdin:
     # The result is a tuple with 2 elements
     data = line.strip().split("\t")
 
-    # Store the 2 elements of this line in seperate variables
+    # Store the 2 elements of this line in separate variables
     key, value = data
 
     # Do we have a previous_key (previous_key != None) and 
     # is the new key different than the previous key?
     # This means the line starts with a new key (key changes e.g. from "Visa" to "Cash")
     # Remember that our keys are sorted
-    if previous_key != None and previous_key != key:
+    if previous_key is not None and previous_key != key:
+        # Then write the result of the old key (Key=category, Value=Average Sales)
+        # to the standard output (stdout)
+        # Key and value are separated by a tab (\t)
+        # Line ends with a new line (\n)
+        average_sales = sum_of_values / count if count > 0 else 0
+        sys.stdout.write("{0}\t{1}\n".format(previous_key, average_sales))
+        
+        # Reset sum of values and count for the new category
+        sum_of_values = 0
+        count = 0
 
-        if purchase_count > 114:
-        	# Then write the result of the old key (Key=category, Value= Sum of Sales)
-        	# to the standart output (stdout)
-        	# Key and value are seperated by a tab (\t)
-        	# Line ends with new line (\n)
-        	sys.stdout.write("{0}\t{1}\n".format(previous_key, purchase_count))
-        # Sum of sales starts again with 0
-        purchase_count = 0
-
-    # Increment the count
-    purchase_count += 1
-    # the previous key for the next iteration is the current key of the this iteration 
+    # Add the value to the total sales
+    # a += b is the same as a = a + b
+    # the float function transforms the value
+    # to a float data type (like decimal)
+    sum_of_values += float(value)
+    # Increment the count for each value
+    count += 1
+    # the previous key for the next iteration is the current key of this iteration 
     previous_key = key
 
 # write the last result to stdout
-sys.stdout.write("{0}\t{1}\n".format(previous_key, purchase_count))
+average_sales = sum_of_values / count if count > 0 else 0
+sys.stdout.write("{0}\t{1}\n".format(previous_key, average_sales))
+
